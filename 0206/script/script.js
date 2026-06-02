3effc6bec60812fd8b142cb51ff07a7b9072f8a0
Arthur Henrique <arthurcoelho6235@gmail.com>
2026-06-02 08:30:27 -0300

    Atividade 02/06

---
+++ 0206/script/script.js
@@ -0,0 +1,11 @@
+var arrayquant = [
+    document.querySelector('#qtd1'),
+    document.querySelector('#qtd2'),
+    document.querySelector('#qtd3')
+]
+var arraytotal = [
+    document.querySelector('#Total1'),
+    document.querySelector('#Total2'),
+    document.querySelector('#Total3')
+]
+var quantidade = [0 , 0 , 0];
